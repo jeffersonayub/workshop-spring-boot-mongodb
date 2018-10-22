d12fbc15cb6a929bd0172514179c3c68e48bbd6b
Jefferson Ayub <jefferson.ayub@gmail.com>
2018-10-22 19:07:07 -0300

    Referenciando os posts do usuário

--- src/main/java/com/jeayub/workshopmongo/domain/User.java
+++ src/main/java/com/jeayub/workshopmongo/domain/User.java
@@ -1,22 +1,26 @@
 package com.jeayub.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection="user")
 public class User implements Serializable {
-
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	private String id;
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
-		
 	}
 
 	public User(String id, String name, String email) {
@@ -50,6 +54,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -73,5 +85,5 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}		
+	}
 }
